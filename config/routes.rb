6e05b68f7b999f260cbeed8eff985f055b00cc4a
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-07-21 04:22:36 +0300

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  #Getting all plants to display
+  get '/plants', to: 'plants#index'
+
+
+  #Getting one plant by an id
+  get '/plants/:id', to:'plants#show'
+
+  #Adding a new plant in the database
+  post '/plants', to: 'plants#create'
 end
